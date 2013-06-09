13f16ea24c20aab0e2995cee9418000dc25f61b4
evanhuang8 <evanhuang8@gmail.com>
2013-06-09 02:51:41 -0700

    Working environment setup

--- Bazaarboy/Bazaarboy/settings.py
+++ Bazaarboy/Bazaarboy/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'Bazaarboy',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'L33che$',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -113,12 +113,14 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
+    #'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'kernel',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
 """
 Installation
+-- LAMP (sudo apt-get install tasksel, then tasksel)
+-- phpmyadmin (sudo apt-get install phpmyadmin) (Include /etc/phpmyadmin/apache.conf in /etc/apache2/apache2.conf)
 -- pip (sudo apt-get install python-pip)
 -- virtualenvwrapper (sudo pip install virtualenvwrapper)
   -- Edit ~/.bashrc
@@ -10,10 +12,22 @@ Installation
       Reactivate bash profile
         source ~/.bashrc
   -- mkvirtualenv bazaarboy
+-- python-dev
+-- mysql-python
 -- django 1.4.5 (pip install Django==1.4.5)
 -- django-admin.py startproject Bazaarboy
 -- git (sudo apt-get install git)
 -- git init
 -- Edit .gitignore
   *.pyc *.swp *.log *~ 
+-- south (pip install south==0.8.1)
+-- fabric (pip install fabric==1.6.1)
 """
+
+from fabric.api import settings, lcd, local
+
+def run():
+  with settings(warn_only=True):
+    with lcd('./Bazaarboy/'):
+      local('python manage.py schemamigration kernel --auto')
+      local('python manage.py runserver 0.0.0.0:8080')
