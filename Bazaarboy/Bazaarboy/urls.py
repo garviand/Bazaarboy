86988477df8cef2f87b0090c9c24a8fee77d782e
evanhuang8 <evanhuang8@gmail.com>
2013-07-20 14:44:14 -0700

    separated url patterns

--- Bazaarboy/Bazaarboy/urls.py
+++ Bazaarboy/Bazaarboy/urls.py
@@ -1,119 +1,84 @@
 from django.conf.urls import patterns, include, url
 
+index_urlpatterns = patterns('src.controllers.index', 
+    url(r'^$', 'index', name = 'index'),
+)
+
+user_urlpatterns = patterns('src.controllers.user', 
+    url(r'^register/$', 'register', name = 'register'),
+    url(r'^create/$', 'create', name = 'create'),
+    url(r'^login/$', 'login', name = 'login'),
+    url(r'^auth/$', 'auth', name = 'auth'),
+    url(r'^fbAuth/$', 'fbAuth', name = 'fbAuth'),
+    url(r'^logout/$', 'logout', name = 'logout'),
+)
+
+wepay_urlpatterns = patterns('src.controllers.wepay', 
+    url(r'^authorize/$', 'authorize', name = 'authorize'),
+    url(r'^create/$', 'create', name = 'create'),
+)
+
+city_urlpatterns = patterns('src.controllers.city',
+    url(r'^(?P<id>\d+)/$', 'index', name = 'index'),
+    url(r'^$', 'city', name = 'city'),
+    url(r'^create/$', 'create', name = 'create'),
+    url(r'^edit/$', 'edit', name = 'edit'),
+    url(r'^delete/$', 'delete', name = 'delete'),
+)
+
+community_urlpatterns = patterns('src.controllers.community', 
+    url(r'^(?P<id>\d+)/$', 'index', name = 'index'),
+    url(r'^$', 'community', name = 'community'),
+    url(r'^create/$', 'create', name = 'create'),
+    url(r'^edit/$', 'edit', name = 'edit'),
+    url(r'^delete/$', 'delete', name = 'delete'),
+)
+
+profile_urlpatterns = patterns('src.controllers.profile', 
+    url(r'^(?P<id>\d+)/$', 'index', name = 'index'),
+    url(r'^$', 'profile', name = 'profile'),
+    url(r'^create/$', 'create', name = 'create'),
+    url(r'^edit/$', 'edit', name = 'edit'),
+    url(r'^delete/$', 'delete', name = 'delete'),
+)
+
+event_urlpatterns = patterns('src.controllers.event', 
+    url(r'^(?P<id>\d+)/$', 'index', name = 'index'),
+    url(r'^$', 'event', name = 'event'),
+    url(r'^create/$', 'create', name = 'create'),
+    url(r'^edit/$', 'edit', name = 'edit'),
+    url(r'^delete/$', 'delete', name = 'delete'),
+    url(r'^launch/$', 'launch', name = 'launch'),
+    url(r'^delaunch/$', 'delaunch', name = 'delaunch'),
+    url(r'^ticket/create/$', 'create_ticket', name = 'ticket-create'),
+    url(r'^ticket/edit/$', 'edit_ticket', name = 'ticket-edit'),
+    url(r'^ticket/delete/$', 'delete_ticket', name = 'ticket-delete'),
+    url(r'^purchase/$', 'purchase', name = 'purchase'),
+)
+
+fundraiser_urlpatterns = patterns('src.controllers.fundraiser', 
+    url(r'^(?P<id>\d+)/$', 'index', name = 'index'),
+    url(r'^$', 'fundraiser', name = 'fundraiser'),
+    url(r'^create/$', 'create', name = 'create'),
+    url(r'^edit/$', 'edit', name = 'edit'),
+    url(r'^delete/$', 'delete', name = 'delete'),
+    url(r'^launch/$', 'launch', name = 'launch'),
+    url(r'^delaunch/$', 'delaunch', name = 'delaunch'),
+    url(r'^reward/create/$', 'create_reward', name = 'reward-create'),
+    url(r'^reward/edit/$', 'edit_reward', name = 'reward-edit'),
+    url(r'^reward/delete/$', 'delete_reward', name = 'reward-delete'),
+    url(r'^donate/$', 'donate', name = 'donate'),
+)
+
 urlpatterns = patterns('',
-
-    # Index
-
-    url(r'^$', 'src.controllers.index.index', name = 'index'),
-
-    # User
-
-    url(r'^user/register/$', 'src.controllers.user.register', 
-        name = 'user-register'),
-    url(r'^user/create/$', 'src.controllers.user.create', 
-        name = 'user-create'),
-    url(r'^user/login/$', 'src.controllers.user.login', name = 'user-login'),
-    url(r'^user/auth/$', 'src.controllers.user.auth', name = 'user-auth'),
-    url(r'^user/fbAuth/$', 'src.controllers.user.fbAuth', name = 'user-fbAuth'),
-    url(r'^user/logout/$', 'src.controllers.user.logout', name = 'user-logout'),
-
-    # WePay
-
-    url(r'^wepay/authorize/$', 'src.controllers.wepay.authorize', 
-        name = 'wepay-authorize'),
-    url(r'^wepay/create/$', 'src.controllers.wepay.create', 
-        name = 'wepay-create'),
-    url(r'^wepay/checkout/(?P<id>\d+)/(?P<refType>p|d)/(?P<refId>\d+)/$', 
-        'src.controllers.wepay.checkout',  
-        name = 'wepay-checkout'),
-    url(r'^wepay/checkout/confirm/(?P<id>\d+)/(?P<refType>p|d)/(?P<refId>\d+)/$', 
-        'src.controllers.wepay.confirm_checkout', 
-        name = 'wepay-checkout-confirm'),
-
-    # City
-
-    url(r'^city/(?P<id>\d+)/$', 'src.controllers.city.index', 
-        name = 'city-index'),
-    url(r'^city/$', 'src.controllers.city.city', name = 'city'),
-    url(r'^city/create/$', 'src.controllers.city.create', 
-        name = 'city-create'),
-    url(r'^city/edit/$', 'src.controllers.city.edit', name = 'city-edit'),
-    url(r'^city/delete/$', 'src.controllers.city.delete', 
-        name = 'city-delete'),
-
-    # Community
-
-    url(r'^community/(?P<id>\d+)/$', 'src.controllers.community.index', 
-        name = 'community-index'),
-    url(r'^community/$', 'src.controllers.community.community', 
-        name = 'community'),
-    url(r'^community/create/$', 'src.controllers.community.create', 
-        name = 'community-create'),
-    url(r'^community/edit/$', 'src.controllers.community.edit', 
-        name = 'community-edit'),
-    url(r'^community/delete/$', 'src.controllers.community.delete', 
-        name = 'community-delete'),
-
-    # Profile
-
-    url(r'^profile/(?P<id>\d+)/$', 'src.controllers.profile.index', 
-        name = 'profile-index'),
-    url(r'^profile/$', 'src.controllers.profile.profile', name = 'profile'),
-    url(r'^profile/create/$', 'src.controllers.profile.create', 
-        name = 'profile-create'),
-    url(r'^profile/edit/$', 'src.controllers.profile.edit', 
-        name = 'profile-edit'),
-    url(r'^profile/delete/$', 'src.controllers.profile.delete', 
-        name = 'profile-delete'),
-
-    # Event
-
-    url(r'^event/(?P<id>\d+)/$', 'src.controllers.event.index', 
-        name = 'event-index'),
-    url(r'^event/$', 'src.controllers.event.event', name = 'event'),
-    url(r'^event/create/$', 'src.controllers.event.create', 
-        name = 'event-create'),
-    url(r'^event/edit/$', 'src.controllers.event.edit', name = 'event-edit'),
-    url(r'^event/delete/$', 'src.controllers.event.delete', 
-        name = 'event-delete'),
-    url(r'^event/launch/$', 'src.controllers.event.launch', 
-        name = 'event-launch'),
-    url(r'^event/delaunch/$', 'src.controllers.event.delaunch', 
-        name = 'event-delaunch'),
-    url(r'^event/ticket/create/$', 'src.controllers.event.create_ticket', 
-        name = 'event-ticket-create'),
-    url(r'^event/ticket/edit/$', 'src.controllers.event.edit_ticket', 
-        name = 'event-ticket-edit'),
-    url(r'^event/ticket/delete/$', 'src.controllers.event.delete_ticket', 
-        name = 'event-ticket-delete'),
-    url(r'^event/purchase/$', 'src.controllers.event.purchase', 
-        name = 'event-purchase'),
-
-    # Fundraiser
-
-    url(r'^fundraiser/(?P<id>\d+)/$', 'src.controllers.fundraiser.index', 
-        name = 'fundraiser-index'),
-    url(r'^fundraiser/$', 'src.controllers.fundraiser.fundraiser', 
-        name = 'fundraiser'),
-    url(r'^fundraiser/create/$', 'src.controllers.fundraiser.create', 
-        name = 'fundraiser-create'),
-    url(r'^fundraiser/edit/$', 'src.controllers.fundraiser.edit', 
-        name = 'fundraiser-edit'),
-    url(r'^fundraiser/delete/$', 'src.controllers.fundraiser.delete', 
-        name = 'fundraiser-delete'),
-    url(r'^fundraiser/launch/$', 'src.controllers.fundraiser.launch', 
-        name = 'fundraiser-launch'),
-    url(r'^fundraiser/delaunch/$', 'src.controllers.fundraiser.delaunch', 
-        name = 'fundraiser-delaunch'),
-    url(r'^fundraiser/reward/create/$', 
-        'src.controllers.fundraiser.create_reward', 
-        name = 'fundraiser-reward-create'),
-    url(r'^fundraiser/reward/edit/$', 'src.controllers.fundraiser.edit_reward', 
-        name = 'fundraiser-reward-edit'),
-    url(r'^fundraiser/reward/delete/$', 
-        'src.controllers.fundraiser.delete_reward', 
-        name = 'fundraiser-reward-delete'),
-    url(r'^fundraiser/donate/$', 'src.controllers.fundraiser.donate', 
-        name = 'fundraiser-donate'),
-
+    url(r'^', include(index_urlpatterns, namespace = 'index')),
+    url(r'^user/', include(user_urlpatterns, namespace = 'user')),
+    url(r'^wepay/', include(wepay_urlpatterns, namespace = 'wepay')),
+    url(r'^city/', include(city_urlpatterns, namespace = 'city')),
+    url(r'^community/', 
+        include(community_urlpatterns, namespace = 'community')),
+    url(r'^profile/', include(profile_urlpatterns, namespace = 'profile')),
+    url(r'^event/', include(event_urlpatterns, namespace = 'event')),
+    url(r'^fundraiser/', 
+        include(fundraiser_urlpatterns, namespace = 'fundraiser')),
 )
