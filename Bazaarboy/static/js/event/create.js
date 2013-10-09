13381e9ec2a529a92f80d540d4dd1139a1f3cd3e
Andy Garvin <andy@bazaarboy.com>
2013-10-09 13:21:15 -0500

    Timezone Fix On New Event

--- Bazaarboy/static/js/event/create.js
+++ Bazaarboy/static/js/event/create.js
@@ -2,9 +2,12 @@
   Bazaarboy.event.create = {
     init: function() {
       $('div#event_create form.create').submit(function(event) {
-        var data;
+        var data, local_time, utc_time;
         event.preventDefault();
         data = $(this).serializeObject();
+        local_time = moment(data['start_time']);
+        utc_time = moment.utc(local_time);
+        data['start_time'] = utc_time.format("YYYY-MM-DD HH:mm:ss");
         Bazaarboy.post('event/create/', data, function(response) {
           var eventUrl;
           if (response.status === 'OK') {
--- Bazaarboy/views/js/event/create.coffee
+++ Bazaarboy/views/js/event/create.coffee
@@ -3,6 +3,9 @@ Bazaarboy.event.create =
         $('div#event_create form.create').submit (event) ->
             event.preventDefault()
             data = $(this).serializeObject()
+            local_time = moment(data['start_time'])
+            utc_time = moment.utc(local_time)
+            data['start_time'] = utc_time.format("YYYY-MM-DD HH:mm:ss")
             Bazaarboy.post 'event/create/', data, (response) ->
                 if response.status is 'OK'
                     eventUrl = 'event/' + response.event.pk
