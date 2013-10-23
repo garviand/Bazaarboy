ba0ac199cef3a26636a2af62c35c3abd1a208c4f
Andy Garvin <andy@bazaarboy.com>
2013-10-23 12:44:52 -0500

    Event Manage w/o Checkin

--- Bazaarboy/static/js/event/manage.js
+++ Bazaarboy/static/js/event/manage.js
@@ -1,6 +1,37 @@
 (function() {
   Bazaarboy.event.manage = {
-    init: function() {}
+    filterGuests: function(param, value) {
+      var i, length, rsvp, targetValue, _i, _ref;
+      length = $('div.guest').length;
+      for (i = _i = 0, _ref = length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+        rsvp = $('div.guest:eq(' + i + ')');
+        targetValue = $(rsvp).find('div.' + param).html();
+        if (targetValue.toLowerCase().indexOf(value.toLowerCase()) !== -1) {
+          $(rsvp).removeClass('hidden');
+        }
+      }
+    },
+    init: function() {
+      var _this = this;
+      $('form.list_search input[name=guest_name]').keyup(function(e) {
+        e.preventDefault();
+        if ($('form.list_search input[name=guest_name]').val() === '') {
+          return $('div.guest').removeClass('hidden');
+        } else {
+          $('div.guest').addClass('hidden');
+          return _this.filterGuests('name', $('form.list_search input[name=guest_name]').val());
+        }
+      });
+      $('form.list_search input[name=guest_code]').keyup(function(e) {
+        e.preventDefault();
+        if ($('form.list_search input[name=guest_code]').val() === '') {
+          return $('div.guest').removeClass('hidden');
+        } else {
+          $('div.guest').addClass('hidden');
+          return _this.filterGuests('confirmation', $('form.list_search input[name=guest_code]').val());
+        }
+      });
+    }
   };
 
   Bazaarboy.event.manage.init();
--- Bazaarboy/views/js/event/manage.coffee
+++ Bazaarboy/views/js/event/manage.coffee
@@ -1,5 +1,27 @@
-Bazaarboy.event.manage = 
+Bazaarboy.event.manage =
+    filterGuests: (param, value) ->
+        length = $('div.guest').length
+        for i in [0..length-1]
+            rsvp = $('div.guest:eq(' + i + ')')
+            targetValue = $(rsvp).find('div.' + param).html()
+            if targetValue.toLowerCase().indexOf(value.toLowerCase()) != -1
+                $(rsvp).removeClass('hidden')
+        return
     init: () ->
+        $('form.list_search input[name=guest_name]').keyup (e) =>
+            e.preventDefault()
+            if $('form.list_search input[name=guest_name]').val() == ''
+                $('div.guest').removeClass('hidden')
+            else
+                $('div.guest').addClass('hidden')
+                @filterGuests('name', $('form.list_search input[name=guest_name]').val())
+        $('form.list_search input[name=guest_code]').keyup (e) =>
+            e.preventDefault()
+            if $('form.list_search input[name=guest_code]').val() == ''
+                $('div.guest').removeClass('hidden')
+            else
+                $('div.guest').addClass('hidden')
+                @filterGuests('confirmation', $('form.list_search input[name=guest_code]').val())
         return
 
 Bazaarboy.event.manage.init()
