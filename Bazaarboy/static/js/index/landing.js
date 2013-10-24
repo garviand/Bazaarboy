46ae7094078d5f173a58e90bd806e2833bf33315
Andy Garvin <andy@bazaarboy.com>
2013-10-24 15:16:05 -0500

    Landing with Login

--- Bazaarboy/static/js/index/landing.js
+++ Bazaarboy/static/js/index/landing.js
@@ -1,6 +1,39 @@
 (function() {
   this.Bazaarboy.landing = {
-    init: function() {}
+    init: function() {
+      $('form[name=login] input').keypress(function(event) {
+        if (event.which === 13) {
+          event.preventDefault();
+          $('form[name=login]').submit();
+        }
+      });
+      $('#landing .inner .login_content .bottom a.login_btn').click(function() {
+        $('form[name=login]').submit();
+      });
+      $('form[name=login]').submit(function(event) {
+        var params;
+        console.log("log in now");
+        event.preventDefault();
+        params = $('form[name=login]').serialize();
+        Bazaarboy.get('user/auth/', params, function(response) {
+          if (response.status === 'OK') {
+            return Bazaarboy.redirect('index');
+          } else {
+            return alert(response.message);
+          }
+        });
+      });
+      $('#landing .inner .starting_content .bottom .login_link_container .start_sign_in').click(function() {
+        $('#landing .inner .starting_content').addClass('hidden');
+        $('#landing .inner .login_content').removeClass('hidden');
+      });
+      $('#landing .inner .login_content .bottom a.back').click(function() {
+        $('#landing .inner .login_content').addClass('hidden');
+        $('#landing .inner .starting_content').removeClass('hidden');
+      });
+    }
   };
+
+  Bazaarboy.landing.init();
 
 }).call(this);
--- Bazaarboy/views/js/index/landing.coffee
+++ Bazaarboy/views/js/index/landing.coffee
@@ -1,3 +1,31 @@
 @Bazaarboy.landing = 
     init: () ->
+    	$('form[name=login] input').keypress (event) ->
+            if event.which == 13
+                event.preventDefault()
+                $('form[name=login]').submit()
+            return
+        $('#landing .inner .login_content .bottom a.login_btn').click () ->
+            $('form[name=login]').submit()
+            return
+        $('form[name=login]').submit (event) ->
+            console.log "log in now"
+            event.preventDefault()
+            params = $('form[name=login]').serialize()
+            Bazaarboy.get 'user/auth/', params, (response) ->
+                if response.status is 'OK'
+                    Bazaarboy.redirect 'index'
+                else
+                    alert response.message
+            return
+        $('#landing .inner .starting_content .bottom .login_link_container .start_sign_in').click () ->
+            $('#landing .inner .starting_content').addClass 'hidden'
+            $('#landing .inner .login_content').removeClass 'hidden'
+            return
+        $('#landing .inner .login_content .bottom a.back').click () ->
+            $('#landing .inner .login_content').addClass 'hidden'
+            $('#landing .inner .starting_content').removeClass 'hidden'
+            return
         return
+
+Bazaarboy.landing.init()
