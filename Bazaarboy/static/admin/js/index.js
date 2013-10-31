4fe859736621402ac37a82d4445d9b66c198c4a7
Andy Garvin <andy@bazaarboy.com>
2013-10-31 13:34:03 -0500

    Admin Login as Profile Manager

--- Bazaarboy/static/admin/js/index.js
+++ Bazaarboy/static/admin/js/index.js
@@ -1,22 +1,43 @@
 (function() {
   Bazaarboy.admin.login = {
-    init: function() {}
+    filterProfiles: function(value) {
+      var i, length, profile, targetValue, _i, _ref;
+      length = $('.profile_login .profile_choices a').length;
+      for (i = _i = 0, _ref = length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+        profile = $('.profile_login .profile_choices a:eq(' + i + ')');
+        targetValue = $(profile).html();
+        if (targetValue.toLowerCase().indexOf(value.toLowerCase()) !== -1) {
+          $(profile).removeClass('hidden');
+        }
+      }
+    },
+    init: function() {
+      var _this = this;
+      $('.profile_login .profile_choices a').click(function(event) {
+        var id;
+        id = $(this).data('id');
+        Bazaarboy.get('admin/login/profile', {
+          id: id
+        }, function(response) {
+          if (response.status === 'OK') {
+            Bazaarboy.redirect('index');
+          } else {
+            alert(response.message);
+          }
+        });
+      });
+      $('.profile_login .input_container input[name=profile_name').keyup(function(event) {
+        event.preventDefault();
+        if ($('.profile_login .input_container input[name=profile_name').val() === '') {
+          $('.profile_login .profile_choices a').removeClass('hidden');
+        } else {
+          $('.profile_login .profile_choices a').addClass('hidden');
+          _this.filterProfiles($('.profile_login .input_container input[name=profile_name').val());
+        }
+      });
+    }
   };
 
-  $('.profile_login .profile_choices a').click(function(event) {
-    var id;
-    id = $(this).data('id');
-    Bazaarboy.get('admin/login/profile', {
-      id: id
-    }, function(response) {
-      if (response.status === 'OK') {
-        Bazaarboy.redirect('index');
-      } else {
-        alert(response.message);
-      }
-    });
-  });
-
-  return;
+  Bazaarboy.admin.login.init();
 
 }).call(this);
--- Bazaarboy/views/admin/js/index.coffee
+++ Bazaarboy/views/admin/js/index.coffee
@@ -1,5 +1,13 @@
-Bazaarboy.admin.login = 
-    init: () ->
+Bazaarboy.admin.login =
+	filterProfiles: (value) ->
+		length = $('.profile_login .profile_choices a').length
+		for i in [0..length-1]
+			profile = $('.profile_login .profile_choices a:eq(' + i + ')')
+			targetValue = $(profile).html()
+			if targetValue.toLowerCase().indexOf(value.toLowerCase()) != -1
+				$(profile).removeClass('hidden')
+		return
+	init: () ->
 		$('.profile_login .profile_choices a').click (event) ->
 			id = $(this).data('id')
 			Bazaarboy.get 'admin/login/profile', {id:id}, (response) ->
@@ -9,4 +17,14 @@ Bazaarboy.admin.login =
 					alert response.message
 				return
 			return
+		$('.profile_login .input_container input[name=profile_name').keyup (event) =>
+			event.preventDefault()
+			if $('.profile_login .input_container input[name=profile_name').val() == ''
+				$('.profile_login .profile_choices a').removeClass('hidden')
+			else
+				$('.profile_login .profile_choices a').addClass('hidden')
+				@filterProfiles($('.profile_login .input_container input[name=profile_name').val())
+			return
 		return
+
+Bazaarboy.admin.login.init()
