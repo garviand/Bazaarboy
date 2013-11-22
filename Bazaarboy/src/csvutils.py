7af12c2e6ce9267659086c617441e7a01a9be504
evanhuang8 <evanhuang8@gmail.com>
2013-11-22 14:32:46 -0600

    CSV utils

--- Bazaarboy/src/csvutils.py
+++ Bazaarboy/src/csvutils.py
@@ -1,31 +1,25 @@
 """
-CSV Export utilities
+CSV export utilities
 """
 
 import csv, codecs, cStringIO
 
 class UnicodeWriter:
     """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
+    Write unicode content to stream in CSV format
     """
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
+    def __init__(self, f, dialect=csv.excel, encoding='utf-8', **kwds):
         self.queue = cStringIO.StringIO()
         self.writer = csv.writer(self.queue, dialect = dialect, **kwds)
         self.stream = f
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
+        self.writer.writerow([s.encode('utf-8') for s in row])
         data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
+        data = data.decode('utf-8')
         data = self.encoder.encode(data)
-        # write to the target stream
         self.stream.write(data)
-        # empty queue
         self.queue.truncate(0)
 
     def writerows(self, rows):
