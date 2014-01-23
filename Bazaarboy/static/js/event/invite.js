a78a61b934fc30fe32a1faa887cc4902835c43e3
Andy Garvin <garvin.andy@gmail.com>
2014-01-23 21:21:06 +0000

    Invite Email Editor v1.1

--- Bazaarboy/static/js/event/invite.js
+++ Bazaarboy/static/js/event/invite.js
@@ -1,15 +1,22 @@
 (function() {
   Bazaarboy.event.invite = {
     init: function() {
-      var options;
-      options = {
+      var typewatch_options;
+      typewatch_options = {
         callback: function(value) {
           $(".template_container #organizer_message").html($("form.email_form textarea[name=message]").val());
         },
         wait: 750,
         highlight: true
       };
-      $("form.email_form textarea[name=message]").typeWatch(options);
+      $("form.email_form textarea[name=message]").typeWatch(typewatch_options);
+      $('input[name=include_cover]').change(function() {
+        if ($(this).is(':checked')) {
+          $(".template_container #cover_image").show();
+        } else {
+          $(".template_container #cover_image").hide();
+        }
+      });
     }
   };
 
--- Bazaarboy/views/js/event/invite.coffee
+++ Bazaarboy/views/js/event/invite.coffee
@@ -1,12 +1,18 @@
 Bazaarboy.event.invite =
     init: () ->
-        options =
+        typewatch_options =
             callback: (value) ->
                 $(".template_container #organizer_message").html($("form.email_form textarea[name=message]").val())
                 return
             wait: 750
             highlight: true
-        $("form.email_form textarea[name=message]").typeWatch(options)
+        $("form.email_form textarea[name=message]").typeWatch(typewatch_options)
+        $('input[name=include_cover]').change () ->
+            if $(this).is(':checked')
+                $(".template_container #cover_image").show()
+            else
+                $(".template_container #cover_image").hide()
+            return
         return
 
 Bazaarboy.event.invite.init()
