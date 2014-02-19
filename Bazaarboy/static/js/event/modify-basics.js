384d8da6528013836f79b8cb7ab99c23d32e662d
Andy Garvin <garvin.andy@gmail.com>
2014-02-19 22:34:23 +0000

    Date and Time AutoComplete

--- Bazaarboy/static/js/event/modify-basics.js
+++ Bazaarboy/static/js/event/modify-basics.js
@@ -1,6 +1,25 @@
 (function() {
   Bazaarboy.event.modify.basics = {
-    init: function() {}
+    init: function() {
+      var originalEndTime, originalStartTime;
+      originalStartTime = $("form.event-modify input[name=start_time]").val();
+      originalEndTime = $("form.event-modify input[name=end_time]").val();
+      $("form.event-modify input[name=start_time], form.event-modify input[name=end_time]").timeAutocomplete({
+        blur_empty_populate: false
+      });
+      $("form.event-modify input[name=start_time]").val(originalStartTime);
+      $("form.event-modify input[name=end_time]").val(originalEndTime);
+      $('form.event-modify input[name=start_date]').pikaday({
+        format: 'MM/DD/YYYY',
+        onSelect: function() {
+          $('form.event-modify input[name=end_date]').pikaday('gotoDate', this.getDate());
+          $('form.event-modify input[name=end_date]').pikaday('setMinDate', this.getDate());
+        }
+      });
+      $('form.event-modify input[name=end_date]').pikaday({
+        format: 'MM/DD/YYYY'
+      });
+    }
   };
 
   Bazaarboy.event.modify.basics.init();
--- Bazaarboy/views/js/event/modify-basics.coffee
+++ Bazaarboy/views/js/event/modify-basics.coffee
@@ -1,5 +1,21 @@
 Bazaarboy.event.modify.basics = 
     init: () ->
-      return
+        # INIT TIME AUTOCOMPLETE
+        originalStartTime = $("form.event-modify input[name=start_time]").val()
+        originalEndTime = $("form.event-modify input[name=end_time]").val()
+        $("form.event-modify input[name=start_time], form.event-modify input[name=end_time]").timeAutocomplete
+            blur_empty_populate: false
+        $("form.event-modify input[name=start_time]").val(originalStartTime) 
+        $("form.event-modify input[name=end_time]").val(originalEndTime)
+        # INIT DATE AUTOCOMPLETE
+        $('form.event-modify input[name=start_date]').pikaday
+            format: 'MM/DD/YYYY'
+            onSelect: () ->
+                $('form.event-modify input[name=end_date]').pikaday('gotoDate', this.getDate())
+                $('form.event-modify input[name=end_date]').pikaday('setMinDate', this.getDate())
+                return
+        $('form.event-modify input[name=end_date]').pikaday
+            format: 'MM/DD/YYYY'
+        return
 
 Bazaarboy.event.modify.basics.init()
