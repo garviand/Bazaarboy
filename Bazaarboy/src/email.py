41502ea40432e6eadd790a83dc191b644b075674
Andy Garvin <garvin.andy@gmail.com>
2014-06-27 18:46:52 +0000

    Mobile Email Image Size

--- Bazaarboy/src/email.py
+++ Bazaarboy/src/email.py
@@ -274,7 +274,7 @@ def sendEventConfirmationEmail(purchase, manual=False, inviter=None):
     for organizer in organizers:
         if organizer.image:
             organizer_image = """
-                <img width="38" class="organizer_logo" src='""" + organizer.image.source.url + """' style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: auto; max-width: 40px; float: left; clear: both; display: block;" align="left" />
+                <img width="38" class="organizer_logo" src='""" + organizer.image.source.url + """' style="outline: none; text-decoration: none; width:40px; max-width: 40px; float: left; display: block;" align="left" />
             """
         else:
             organizer_image = ''
