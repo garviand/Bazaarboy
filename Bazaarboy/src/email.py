4193a92da18735576eea481bd29996f370d260ff
Andy Garvin <garvin.andy@gmail.com>
2014-10-28 03:38:46 +0000

    Add Name To PDF Ticket

--- Bazaarboy/src/email.py
+++ Bazaarboy/src/email.py
@@ -715,7 +715,8 @@ class Ticket_attachment(Attachment):
             'start_time':event.start_time,
             'end_time':event.end_time,
             'location':event.location,
-            'purchase':purchase.id
+            'purchase':purchase.id,
+            'name':purchase.owner.full_name
         }
         i = 1
         for item in items:
