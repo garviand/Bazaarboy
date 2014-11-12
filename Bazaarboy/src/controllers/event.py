1b95ebaca52a3024a7bf4173da6348b935e3a8ed
Andy Garvin <garvin.andy@gmail.com>
2014-11-12 04:51:23 +0000

    Change WCHOF Page

--- Bazaarboy/src/controllers/event.py
+++ Bazaarboy/src/controllers/event.py
@@ -113,7 +113,7 @@ def index(request, id, params, user):
             rsvp = False
         if ticket.price < cheapest:
             cheapest = ticket.price
-    if design or event.id not in [337, 396]:
+    if design or event.id not in [337, 405]:
         return render(request, 'event/index.html', locals())
     else:
         return render(request, 'event/format-full-screen.html', locals())
