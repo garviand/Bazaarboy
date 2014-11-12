e97e0e37507d319b708230ad026327cb492aed2e
Andy Garvin <garvin.andy@gmail.com>
2014-11-12 17:43:18 +0000

    add spark

--- Bazaarboy/src/controllers/event.py
+++ Bazaarboy/src/controllers/event.py
@@ -113,7 +113,7 @@ def index(request, id, params, user):
             rsvp = False
         if ticket.price < cheapest:
             cheapest = ticket.price
-    if design or event.id not in [337, 405]:
+    if design or event.id not in [337, 405, 378]:
         return render(request, 'event/index.html', locals())
     else:
         return render(request, 'event/format-full-screen.html', locals())
