17856a815d1ddd3d110db2f7f28a552ed358935f
Andy Garvin <garvin.andy@gmail.com>
2015-03-17 23:18:58 +0000

    Keep Old Splash For Now

--- Bazaarboy/static/js/index/landing.js
+++ Bazaarboy/static/js/index/landing.js
@@ -1,58 +1,47 @@
 (function() {
   this.Bazaarboy.landing = {
-    searchEvents: function(value) {
-      return Bazaarboy.get('profile/search/', {
-        keyword: value
-      }, function(response) {
-        var profiles;
-        if (response.status === 'OK') {
-          profiles = response.profiles;
-          if (profiles.length > 0) {
-            return profiles;
-          } else {
-            return [];
-          }
+    init: function() {
+      $('form.login input').keypress(function(event) {
+        if (event.which === 13) {
+          event.preventDefault();
+          $('form.login').submit();
         }
       });
-    },
-    init: function() {
-      $('input[name=event_name]').autocomplete({
-        html: true,
-        source: function(request, response) {
-          Bazaarboy.get('event/search/', {
-            keyword: request.term
-          }, function(results) {
-            var events, evnt, thisLabel, _i, _len, _ref;
-            events = [];
-            _ref = results.events;
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              evnt = _ref[_i];
-              thisLabel = '<div class="autocomplete_result row" data-id="' + evnt.pk + '">';
-              if (evnt.image_url != null) {
-                thisLabel += '<div class="small-2 columns autocomplete_image" style="background-image:url(' + evnt.image_url + '); background-size:contain; background-position:center; background-repeat:no-repeat;" />';
-              }
-              thisLabel += '<div class="small-10 columns autocomplete_name">' + evnt.name + '</div>';
-              thisLabel += '</div>';
-              events.push({
-                label: thisLabel,
-                value: evnt.name
-              });
+      $('form.login').submit(function(event) {
+        var params;
+        event.preventDefault();
+        params = $('form.login').serializeObject();
+        params = Bazaarboy.trim(params);
+        if (params.email.length !== 0 && params.password.length !== 0) {
+          Bazaarboy.post('user/auth/', params, function(response) {
+            if (response.status === 'OK') {
+              Bazaarboy.redirect('index');
+            } else {
+              alert(response.message);
             }
-            return response(events);
           });
         }
       });
-      $('div.slider-container div.organizer-slider').slick({
-        arrows: false
+      $('form.register input').keypress(function(event) {
+        if (event.which === 13) {
+          event.preventDefault();
+          $('form.register').submit();
+        }
       });
-      $('ul.slider-controls div.logo-container a').click(function() {
-        var slideNum;
-        slideNum = $(this).closest('div.logo-container').data('slidenum');
-        $('div.slider-container div.organizer-slider').slick('slickGoTo', slideNum);
-      });
-      $('div.slider-container div.organizer-slider').on('afterChange', function(event, slick, currentSlide, nextSlide) {
-        $('ul.slider-controls div.logo-container').removeClass('active');
-        $('ul.slider-controls div.logo-container[data-slidenum=' + currentSlide + ']').addClass('active');
+      $('form.register').submit(function(event) {
+        var params;
+        event.preventDefault();
+        params = $('form.register').serializeObject();
+        params = Bazaarboy.trim(params);
+        if (params.email.length !== 0 && params.password.length !== 0 && params.password === params.confirm && params.first_name.length !== 0 && params.last_name.length !== 0) {
+          Bazaarboy.post('user/create/', params, function(response) {
+            if (response.status === 'OK') {
+              Bazaarboy.redirect('index');
+            } else {
+              alert(response.message);
+            }
+          });
+        }
       });
     }
   };
--- Bazaarboy/views/js/index/landing.coffee
+++ Bazaarboy/views/js/index/landing.coffee
@@ -1,38 +1,43 @@
 @Bazaarboy.landing = 
-    searchEvents: (value) ->
-        Bazaarboy.get 'profile/search/', {keyword: value}, (response) ->
-            if response.status is 'OK'
-                profiles = response.profiles
-                if profiles.length > 0
-                    return profiles
-                else
-                    return []
-            return
     init: () ->
-        $('input[name=event_name]').autocomplete
-            html: true,
-            source: (request, response) ->
-                Bazaarboy.get 'event/search/', {keyword: request.term}, (results) ->
-                    events = []
-                    for evnt in results.events
-                        thisLabel = '<div class="autocomplete_result row" data-id="' + evnt.pk + '">'
-                        if evnt.image_url?
-                            thisLabel += '<div class="small-2 columns autocomplete_image" style="background-image:url(' + evnt.image_url + '); background-size:contain; background-position:center; background-repeat:no-repeat;" />'
-                        thisLabel += '<div class="small-10 columns autocomplete_name">' + evnt.name + '</div>'
-                        thisLabel += '</div>'
-                        events.push({label: thisLabel, value: evnt.name})
-                    return response(events)
-                return
-        $('div.slider-container div.organizer-slider').slick(
-            arrows: false
-        )
-        $('ul.slider-controls div.logo-container a').click () ->
-            slideNum = $(this).closest('div.logo-container').data('slidenum')
-            $('div.slider-container div.organizer-slider').slick('slickGoTo', slideNum)
+        $('form.login input').keypress (event) ->
+            if event.which == 13
+                event.preventDefault()
+                $('form.login').submit()
             return
-        $('div.slider-container div.organizer-slider').on 'afterChange', (event, slick, currentSlide, nextSlide) ->
-            $('ul.slider-controls div.logo-container').removeClass('active')
-            $('ul.slider-controls div.logo-container[data-slidenum=' + currentSlide + ']').addClass('active')
+        $('form.login').submit (event) ->
+            event.preventDefault()
+            params = $('form.login').serializeObject()
+            params = Bazaarboy.trim params
+            if params.email.length isnt 0 and
+                params.password.length isnt 0
+                    Bazaarboy.post 'user/auth/', params, (response) ->
+                        if response.status is 'OK'
+                            Bazaarboy.redirect 'index'
+                        else
+                            alert response.message
+                        return
+            return
+        $('form.register input').keypress (event) ->
+            if event.which == 13
+                event.preventDefault()
+                $('form.register').submit()
+            return
+        $('form.register').submit (event) ->
+            event.preventDefault()
+            params = $('form.register').serializeObject()
+            params = Bazaarboy.trim params
+            if params.email.length isnt 0 and
+                params.password.length isnt 0 and
+                params.password is params.confirm and
+                params.first_name.length isnt 0 and
+                params.last_name.length isnt 0
+                    Bazaarboy.post 'user/create/', params, (response) ->
+                        if response.status is 'OK'
+                            Bazaarboy.redirect 'index'
+                        else
+                            alert response.message
+                        return
             return
         return
 
