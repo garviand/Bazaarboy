4db3d35faabf1e1775e0f33874dfb04ba731fb30
Andy Garvin <garvin.andy@gmail.com>
2015-04-09 01:27:26 +0000

    New Claim

--- Bazaarboy/static/js/reward/claim.js
+++ Bazaarboy/static/js/reward/claim.js
@@ -4,31 +4,42 @@
     init: function() {
       var scope;
       scope = this;
+      $('div.claim-views').slick({
+        adaptiveHeight: true,
+        arrows: false,
+        draggable: false,
+        infinite: false,
+        swipe: false,
+        speed: 150
+      });
+      $('a.claim-next').click(function() {
+        $('div.claim-views').slick('slickNext');
+      });
       $('form#claim-form').submit(function(e) {
         var extra_fields, field_missing, field_missing_name, params;
         e.preventDefault();
         if (!scope.claiming) {
           scope.claiming = true;
-          $('input.submit-claim').val('Claiming...');
+          $('input.submit-claim').val('...');
           params = $('form#claim-form').serializeObject();
           if (params.phone.trim() === '') {
             params.phone = void 0;
           }
           if (params.first_name === '') {
             swal('Must Enter A First Name');
-            $('input.submit-claim').val('Claim Reward!');
+            $('input.submit-claim').val('Claim!');
             scope.claiming = false;
             return;
           }
           if (params.last_name === '') {
             swal('Must Enter A Last Name');
-            $('input.submit-claim').val('Claim Reward!');
+            $('input.submit-claim').val('Claim!');
             scope.claiming = false;
             return;
           }
           if (params.email === '') {
             swal('Must Enter An Email');
-            $('input.submit-claim').val('Claim Reward!');
+            $('input.submit-claim').val('Claim!');
             scope.claiming = false;
             return;
           }
@@ -45,16 +56,16 @@
           });
           if (field_missing) {
             swal(field_missing_name + ' Cannot Be Blank');
-            $('input.submit-claim').val('Claim Reward!');
+            $('input.submit-claim').val('Claim!');
             scope.claiming = false;
             return;
           }
           params.extra_fields = JSON.stringify(extra_fields);
           Bazaarboy.post('rewards/claim/complete/', params, function(response) {
             scope.claiming = false;
-            $('input.submit-claim').val('Claim Reward!');
+            $('input.submit-claim').val('Claim!');
             if (response.status === 'OK') {
-              $('div.claim-inputs').addClass('hide');
+              $('div.claim-views').css('display', 'none');
               return $('div.claim-success').removeClass('hide');
             } else {
               return swal(response.message);
--- Bazaarboy/views/js/reward/claim.coffee
+++ Bazaarboy/views/js/reward/claim.coffee
@@ -2,27 +2,37 @@ Bazaarboy.reward.claim =
   claiming:false
   init: () ->
     scope = this
+    $('div.claim-views').slick
+      adaptiveHeight: true
+      arrows: false
+      draggable: false
+      infinite: false
+      swipe: false
+      speed: 150
+    $('a.claim-next').click () ->
+      $('div.claim-views').slick('slickNext')
+      return
     $('form#claim-form').submit (e) ->
       e.preventDefault()
       if not scope.claiming
         scope.claiming = true
-        $('input.submit-claim').val('Claiming...')
+        $('input.submit-claim').val('...')
         params = $('form#claim-form').serializeObject()
         if params.phone.trim() is ''
           params.phone = undefined
         if params.first_name is ''
           swal 'Must Enter A First Name'
-          $('input.submit-claim').val('Claim Reward!')
+          $('input.submit-claim').val('Claim!')
           scope.claiming = false
           return
         if params.last_name is ''
           swal 'Must Enter A Last Name'
-          $('input.submit-claim').val('Claim Reward!')
+          $('input.submit-claim').val('Claim!')
           scope.claiming = false
           return
         if params.email is ''
           swal 'Must Enter An Email'
-          $('input.submit-claim').val('Claim Reward!')
+          $('input.submit-claim').val('Claim!')
           scope.claiming = false
           return
         field_missing = false
@@ -37,15 +47,15 @@ Bazaarboy.reward.claim =
           return
         if field_missing
           swal field_missing_name + ' Cannot Be Blank'
-          $('input.submit-claim').val('Claim Reward!')
+          $('input.submit-claim').val('Claim!')
           scope.claiming = false
           return
         params.extra_fields = JSON.stringify(extra_fields)
         Bazaarboy.post 'rewards/claim/complete/', params, (response) ->
           scope.claiming = false
-          $('input.submit-claim').val('Claim Reward!')
+          $('input.submit-claim').val('Claim!')
           if response.status is 'OK'
-            $('div.claim-inputs').addClass('hide')
+            $('div.claim-views').css('display', 'none')
             $('div.claim-success').removeClass('hide')
           else
             swal response.message
